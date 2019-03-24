801dca611a163b472c6a8fb9afa91a80d2fca791
Banyc <Banyc@fakeNews.com>
2019-03-24 10:58:53 +0800

    fix thread jam when exiting program

--- SocketTest/SocketListener.vb
+++ SocketTest/SocketListener.vb
@@ -9,6 +9,8 @@ Public Class SocketListener
 
     Private _buildThread As Thread
 
+    Private _listener As Socket  ' for shutdown
+
     ' Incoming data from the client.  
     Private data As String = Nothing
 
@@ -26,40 +28,47 @@ Public Class SocketListener
         _buildThread.Start()
     End Sub
 
-    Private Function Build(ipStr As String, port As Integer)
-        ' Establish the local endpoint for the socket.  
-        ' Dns.GetHostName returns the name of the   
-        ' host running the application.  
-        'Dim ipHostInfo As IPHostEntry = Dns.GetHostEntry(Dns.GetHostName())
-        'Dim ipAddress As IPAddress = ipHostInfo.AddressList(0)
-        'Dim localEndPoint As New IPEndPoint(ipAddress, 11000)
-        Dim ipAddress As IPAddress = IPAddress.Parse(ipStr)
-        Dim localEndPoint As New IPEndPoint(ipAddress, port)
+    Private Function Build(ipStr As String, port As Integer) As Socket
+        Dim handler As Socket
+        Try
+            ' Establish the local endpoint for the socket.  
+            ' Dns.GetHostName returns the name of the   
+            ' host running the application.  
+            'Dim ipHostInfo As IPHostEntry = Dns.GetHostEntry(Dns.GetHostName())
+            'Dim ipAddress As IPAddress = ipHostInfo.AddressList(0)
+            'Dim localEndPoint As New IPEndPoint(ipAddress, 11000)
+            Dim ipAddress As IPAddress = IPAddress.Parse(ipStr)
+            Dim localEndPoint As New IPEndPoint(ipAddress, port)
 
-        ' Create a TCP/IP socket.  
-        Dim listener As New Socket(ipAddress.AddressFamily,
-        SocketType.Stream, ProtocolType.Tcp)
+            ' Create a TCP/IP socket.  
+            _listener = New Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp)
 
-        ' Bind the socket to the local endpoint and   
-        ' listen for incoming connections.  
+            ' Bind the socket to the local endpoint and   
+            ' listen for incoming connections.  
 
-        listener.Bind(localEndPoint)
-        listener.Listen(10)
+            _listener.Bind(localEndPoint)
+            _listener.Listen(10)
 
-        ' Start listening for connections.  
-        Console.WriteLine("Waiting for a connection...")
-        ' Program is suspended while waiting for an incoming connection.  
-        Dim handler As Socket = listener.Accept()
-
-
+            ' Start listening for connections.  
+            Console.WriteLine("Waiting for a connection...")
+            ' Program is suspended while waiting for an incoming connection.  
+            handler = _listener.Accept()
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString(), SocketCS.Server.ToString())
+            Shutdown()
+        End Try
         Return handler
     End Function
 
     Public Overrides Sub Shutdown()
         MyBase.Shutdown()
+        If Not _listener Is Nothing Then
+            _listener.Close()  ' to kill `_listener.Accept()`, freeing the thread
+        End If
         If _buildThread.IsAlive Then
             _buildThread.Abort()
         End If
     End Sub
 
-End Class 'SynchronousSocketListener  
+End Class 'SocketListener  
