12666d8e3db6bfb150217a663c953c5e6db5cb4d
Banyc <Banyc@fakeNews.com>
2019-08-27 11:37:42 +0800

    Change namespace

--- Chatter/My Project/Settings.Designer.vb
+++ Chatter/My Project/Settings.Designer.vb
@@ -14,7 +14,7 @@ Option Explicit On
 
 
 <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
- Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0"),  _
+ Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0"),  _
  Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
 Partial Friend NotInheritable Class MySettings
     Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -62,9 +62,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.SocketTest.MySettings
+        Friend ReadOnly Property Settings() As Global.Chatter.MySettings
             Get
-                Return Global.SocketTest.MySettings.Default
+                Return Global.Chatter.MySettings.Default
             End Get
         End Property
     End Module
